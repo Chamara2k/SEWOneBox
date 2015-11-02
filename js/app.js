27c4690769dfb00301c5fc881ce8e92b64019cb7
Wicki <chamara2k@gmail.com>
2015-11-02 18:34:23 +1100

    test
    test desc

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
-$('#btnSubmit').click(function(){
-	alert('Button Pressed !!');
-});
+$(document).ready(function(){
+	$('#btnSubmit').click(function(){
+		alert("Button Pressed !!");
+	});
+
+	$('.collapse').on('show.bs.collapse', function () {
+		$('.collapse.in').collapse('hide');
+	});
+   
+
+    var options = {
+		 zoom: 10,
+		 mapTypeId: google.maps.MapTypeId.ROADMAP,
+		 center: new google.maps.LatLng(-38.072424, 145.307299)
+    };
+	  
+	var map = new google.maps.Map(document.getElementById("map"), options);
+
+	 //navigator.geolocation.getCurrentPosition(function(position){
+		  
+	//}); 
+	 
+ });  
